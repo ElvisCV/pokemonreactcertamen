527314c5d5ef902a82c0c69ce5e3d7c6eba14402
elvis <elvis.candia@inacapmail.cl>
2025-07-07 15:34:16 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import PokemonFetcher from './PokemonFetcher';
+
+function App(){
+  return (
+    <>
+    <h1> ¡Conoce a tus pokemones! </h1>
+    <PokemonFetcher/>
+    </>
+
+  );
+}
+export default App;
